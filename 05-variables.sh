9d7b95d3970322270911d9b14e0d2e73f3e0570d
Pallavi C <info@joindevops.com>
2024-12-31 22:23:23 +0530

    conversation added

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,8 @@ echo "please enter your username::" # text enterned here will be added as value 
 read -s USERNAME # here user name is the variable name
 
 echo "username entered: $USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWORD
+
