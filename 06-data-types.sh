a0a7a9754415a2c5e7fafe72db8e160ce843ac15
Pallavi C <info@joindevops.com>
2025-01-01 07:46:32 +0530

    shell-script

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-NUMBER=$1
-NUMBER=$2
+NUMBER1=$1
+NUMBER2=$2
 
-TIMESTAMP=$(date)
-
+TIMSTAMP=$(date)
 echo "script executed at: $TIMESTAMP"
 
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "sum of $NUMBER1 and $NUMBER2 is: $SUM"
+echo "sum of $NUMBER1 and $number2 is: =$SUM"
+
