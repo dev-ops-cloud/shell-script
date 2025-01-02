ef92b8e72a3982e47436335fec1f99ad4a897182
Pallavi C <info@joindevops.com>
2025-01-02 06:29:22 +0530

    shell-script

---
+++ 07-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("Pusha" "rrr" "devara")
+#index stats from 0,size is 3
+
+echo "First movie: ${MOVIEs[0]}"
+echo "Second movie: ${MOVIEs[1]}"
+echo "Third movie: ${MOVIEs[2]}"
+
+echo "All movies are : ${MOVIES[@]}"
