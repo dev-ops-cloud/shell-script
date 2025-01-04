2d5463296d3400381815c27090036f71ecd0b67b
Pallavi C <info@joindevops.com>
2025-01-04 08:02:16 +0530

    shell-script

--- 13-logs.sh
+++ 13-logs.sh
@@ -20,7 +20,7 @@ VALIDATE(){
      fi
 }  
 
-echo "Script started executed at: $TIMESTAMP" &>>$LOG_FILE
+echo "Script started executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 if [ $USERID -ne 0 ]
 then
@@ -28,11 +28,11 @@ then
     exit 1 #other than 0
  fi
 
- dnf list installed mysql &>>$LOG_FILE
+ dnf list installed mysql &>>$LOG_FILE_NAME
 
  if [ $? -ne 0 ]
  then
-     dnf install mysql -y &>>$LOG_FILE
+     dnf install mysql -y &>>$LOG_FILE_NAME
      VALIDATE $? "Installing MySQL"
 else
           echo -e "MySQL already.... $Y INSTALLED"
@@ -40,11 +40,11 @@ else
     fi
 
 
- dnf list installed git &>>$LOG_FILE
+ dnf list installed git &>>$LOG_FILE_NAME
 
  if [ $? -ne 0 ]
  then
-    dnf install git -y &>>$LOG_FILE
+    dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE  $? "Installing Git"
 else
     echo -e "Git is already ... $Y Installed"
