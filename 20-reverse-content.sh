c85ff13c9e87f410394da83503bc95c599560784
Pallavi C <info@joindevops.com>
2025-01-05 21:45:42 +0530

    assignment-tasks

--- 20-reverse-content.sh
+++ 20-reverse-content.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SCHOOL=( "Test 1" "Test 2" "Test 3" )
+SCHOOL=("Test 1" "Test 2" "Test 3")
 #index starts from 0, size is 3
 
-echo "First exam:: ${Test[0]}"
-echo "second exam:: ${Test[1]}"
-echo "Third exam:: ${Test[2]}"
+echo "First exam: ${Test[0]}"
+echo "second exam: ${Test[1]}"
+echo "Third exam: ${Test[2]}"
 
-echo "All exams are:: ${Test[@]}"
+echo "All exams are: ${Test[@]}"
 
 
