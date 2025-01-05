6b2c8b94f2eb280bd73faf91a2c53738dcf99554
Pallavi C <info@joindevops.com>
2025-01-05 16:42:07 +0530

    assignment-tasks

--- 18-count-word.sh
+++ 18-count-word.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+FILE_NAME="/home/devops/daws-82s/repo/shell-script/18-count-word.sh"
+Number_of_Lines= ' WC --lines < $FILE_NAME '
+Number_of_words= ' wc --word < $FILE_NAME'
+
 echo "I am learning Devops with AWS Course"
 echo "First we finished linux commands"
 echo "we started connecting database, backend and frontend servers"
