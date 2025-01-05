c7ba095484c787a987eeebb15922658a94de69cc
Pallavi C <info@joindevops.com>
2025-01-05 16:22:59 +0530

    assignment-tasks

---
+++ 18-count-word.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "I am learning Devops with AWS Course"
+echo "First we finished linux commands"
+echo "we started connecting database, backend and frontend servers"
+echo "we completed linux project"
+echo "we completed shell-script expense project too"
