e047828510ccffd397004473e4502077d086c77c
Pallavi C <info@joindevops.com>
2025-01-21 11:06:40 +0530

    shell-script

--- file-transpose.sh
+++ file-transpose.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-if [ $# -gt 2 ]
-then
-  echo"System error">&2
-exit 1
-
-if [[ $# -eq 2 && -e "$2" && -r "$2" ]]
-then 
-while read -a line; do
-    for ((i=0; i < "${#line[@]}"; i++)); do
-        a[$i]="${a[$i]} ${line[$i]}"
-    done
-done < $2
-
-for ((i=0; i < ${#a[@]}; i++)); do
-    echo ${a[i]}
-done
-
-fi
+echo1= "Name Age"
+echo2= "suresh 15"
+echo3= "ramesh 20"
