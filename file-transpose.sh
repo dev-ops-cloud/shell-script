204d35550f17e3469bba661027b709228cbf6d39
Pallavi C <info@joindevops.com>
2025-01-21 22:17:44 +0530

    shell-script

--- file-transpose.sh
+++ file-transpose.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-input_file1="name age"
-
-n_cols=$(head -1 "$input_file" | wc -w)
-for i in $(seq 1 "$n_cols"); do
-    echo $(cut -d ' ' -f "$i" "$input_file")
-done
+echo "Name Age"
+echo "suresh 20"
+echo "ramesh 25"
+echo "rushika 21"
