06178bff011d996fd99580a78587161fd15a0a51
Pallavi C <info@joindevops.com>
2025-01-22 18:40:08 +0530

    ansible

--- file-transpose.sh
+++ file-transpose.sh
@@ -4,3 +4,5 @@ echo "Name age" > myfile.txt
 echo "Pallavi 25" >> myfile.txt
 echo "rushika 22" >> myfile.txt
 echo "Deepak 30" >> myfile.txt
+
+awk '{ for (i=1; i<=NF; i++) a[i]= (a[i]? a[i] FS $i: $i) } END{ for (i in a) print a[i] }' myfile.txt
