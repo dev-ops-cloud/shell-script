89b8ab28c4cbfd95d3656ce68d4590fc48d98f78
Pallavi C <info@joindevops.com>
2025-01-22 11:00:37 +0530

    ansible

--- file-transpose.sh
+++ file-transpose.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-echo "Name Age"
-echo "suresh 20"
-echo "ramesh 25"
-echo "rushika 21"
+# echo "Name Age"
+# echo "suresh 20"
+# echo "ramesh 25"
+# echo "rushika 21"
+
+if [ $# -gt 2 ]
+then
+  echo"System error">&2
+exit 1
+
+elif [[ $# -eq 2 && -e "$2" && -r "$2" ]]
+then 
+while read -a line; do
+    for ((i=0; i < "${#line[@]}"; i++)); do
+        a[$i]="${a[$i]} ${line[$i]}"
+    done
+done < $2
+
+for ((i=0; i < ${#a[@]}; i++)); do
+    echo ${a[i]}
+done
+
+
+fi
