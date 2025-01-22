93574415eba701f610cf1c2b2336af2a6e8fa974
Pallavi C <info@joindevops.com>
2025-01-22 22:15:14 +0530

    ansible

--- bulkfile-rename.sh
+++ bulkfile-rename.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-dir="/c/devops/daws-82s"
+echo "123.sh" > myfile.txt
+echo "abc.sh" >> myfile.txt
+echo "456.sh" >> myfile.txt
+echo "xyz.sh" >> myfile.txt
+
+dir="/c/devops/daws-82s/repo/shell-script/bulkfile-rename.sh"
 
 timestamp=$(date +%Y%m%d)
 
 for file in "$dir"/*.txt;
 do
-if [ -f "$file" ];then
-   filename=$(basename "$file")
-   echo "file is there in directry: $file"
+if [ -f "$myfile" ];then
+   filename=$(basename "$myfile")
+   echo "file is there in directry: $myfile"
 
-   mv "$file" "$dir/${timestamp}_$filename"
+   mv "$myfile" "$dir/${timestamp}_$filename"
 
  else
 
