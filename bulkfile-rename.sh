51367839edcc6ebdff79b078aa0f0e527e015ad1
Pallavi C <info@joindevops.com>
2025-01-23 07:01:02 +0530

    ansible

--- bulkfile-rename.sh
+++ bulkfile-rename.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+dir="/c/devops/daws-82s/repo/shell-script"
 
-mv file.sh file.xml
+timestamp=$(date +%Y%m%d)
+
+for file in "$dir"/*.txt;
+do 
+ if [ -f "$file" ]; then
+ 
+  filename=$(basename "$file")
+  echo "file is there in directory: $file"
+
+  mv "$file" "$dir/${timestamp}_$filename"
+
+  else
+       echo "file is not found"
+       exit 1
+
+ fi
+ done
+
+ echo "renaming complete."   
